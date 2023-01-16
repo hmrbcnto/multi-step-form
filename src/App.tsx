a404d553e1f2a88de0e4f43bd05efd18e96246f6
Homer <homer@avionschool.com>
2023-01-16 18:31:33 +0800

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
