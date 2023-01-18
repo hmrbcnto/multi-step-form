40fd317696a5eb2443ac819d41270f8c221988af
Homer <homer@avionschool.com>
2023-01-18 13:35:04 +0800

    feat: added Plan page and refactored content of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Router from './Router';
 
+
+// TODO: Import context API for data persistence
 const App: React.FC = () => {
   return (
     <Router />
--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Form from '../components/Form';
 
 const Plan: React.FC = () => {
   return (
-    <div>
-      Plan!
+    <div className="flex flex-col items-center align-center justify-center w-full h-screen bg-neutral-light_gray"> 
+      <Form />
     </div>
   )
 };
