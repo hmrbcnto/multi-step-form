ef5b32704ddbbdb76e60f245eb286579bf907c85
Homer <homer@avionschool.com>
2023-01-18 13:34:28 +0800

    feat: added constants files

---
+++ src/constants/Steps.ts
@@ -0,0 +1,19 @@
+import { StepsItem } from "../components/Steps/Steps.types"
+export const StepConstants: StepsItem[] = [
+  {
+    stepName: 'Your Info',
+    stepNumber: 1
+  },
+  {
+    stepName: 'Select Plan',
+    stepNumber: 2
+  },
+  {
+    stepName: 'Add-ons',
+    stepNumber: 3
+  },
+  {
+    stepName: 'Summary',
+    stepNumber: 4
+  }
+];
