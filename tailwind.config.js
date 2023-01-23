930286d4210656cff05c1fd95933a5bc4e5b5a53
Homer <homer@avionschool.com>
2023-01-23 18:08:02 +0800

    tailwind config now includes base tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,27 +8,27 @@ module.exports = {
       backgroundImage: {
         'steps-image-desktop': "url('/src/assets/images/bg-sidebar-desktop.svg')",
         'steps-image-mobile': "url('/src/assets/images/bg-sidebar-mobile.svg')"
-      }
-    },
-    colors: {
-      primary: {
-        marine_blue: '#02295a',
-        purplish_blue: '#473dff',
-        pastel_blue: '#adbeff',
-        light_blue: '#bfe2fd',
-        strawberry_red: '#ed3548'
       },
-      neutral: {
-        cool_gray: '#9699ab',
-        light_gray: '#d6d9e6',
-        magnolia: '#f0f6ff',
-        alabaster: '#fafbff',
-        white: '#ffffff'
+      colors: {
+        primary: {
+          marine_blue: '#02295a',
+          purplish_blue: '#473dff',
+          pastel_blue: '#adbeff',
+          light_blue: '#bfe2fd',
+          strawberry_red: '#ed3548'
+        },
+        neutral: {
+          cool_gray: '#9699ab',
+          light_gray: '#d6d9e6',
+          magnolia: '#f0f6ff',
+          alabaster: '#fafbff',
+          white: '#ffffff'
+        }
+      },
+      screens: {
+        // 'mobile': '375px',
+        'desktop': '375px'
       }
-    },
-    screens: {
-      // 'mobile': '375px',
-      'desktop': '375px'
     }
   },
   plugins: [],
