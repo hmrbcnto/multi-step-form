5880cb7c748655ee5dbb40cb3b3337db5be4ea51
Homer <homer@avionschool.com>
2023-01-24 14:03:33 +0800

    DRY-ify summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AddOns } from '../../constants/AddOns';
 import { Plans, PlanType } from '../../constants/Plans';
-import { Plan } from '../../contexts/FormContext.types';
 import { SummaryProps } from './Summary.types';
 
 const Summary: React.FC<SummaryProps> = ({
@@ -13,10 +12,10 @@ const Summary: React.FC<SummaryProps> = ({
 }) => {
   const capitalize = (value: string | undefined) => value ? value[0].toUpperCase() + value.substring(1) : '';
   const matchingPlan = Plans.filter((planObject: PlanType) => plan === planObject?.value)[0];
-  // const paymentPerPeriod = Plans.find((planObject: Plan) => planObject.value === plan.value);
+  const paymentPeriod = capitalize(paymentOption.substring(0, paymentOption.length-2))
   return (
     <div className="w-full flex flex-col">
-      <div className="flex flex-col bg-neutral-alabaster p-6 rounded-xl">
+      <div className="flex flex-col bg-neutral-alabaster p-2 rounded-xl">
         <div className="flex flex-row justify-between items-center align-center border-b border-solid border-neutral-cool_gray pb-8">
           <div className="flex flex-col">
             <p className="desktop:text-2xl text-md text-primary-marine_blue font-extrabold">{capitalize(plan)} ({capitalize(paymentOption)}) </p>
@@ -25,7 +24,7 @@ const Summary: React.FC<SummaryProps> = ({
               onClick={clickChange}
             >Change</p>
           </div>
-          <p className="text-sm desktop:text-xl font-extrabold text-primary-marine_blue">${matchingPlan[`${paymentOption}Value`]}/{capitalize(paymentOption.substring(0, paymentOption.length-2))}</p>
+          <p className="text-sm desktop:text-xl font-extrabold text-primary-marine_blue">${matchingPlan[`${paymentOption}Value`]}/{paymentPeriod}</p>
         </div>
         <br />
         <div className="flex flex-col">
@@ -33,7 +32,6 @@ const Summary: React.FC<SummaryProps> = ({
             // Get corresponding data from addon constants
             const addOnObject = AddOns.filter((addOn) => addOn.value === addOnString)[0];
             let cost;
-            const period = capitalize(paymentOption.substring(0, paymentOption.length-2));
             if (paymentOption === 'yearly') {
               cost = addOnObject.yearlyCost;
             } else {
@@ -42,15 +40,15 @@ const Summary: React.FC<SummaryProps> = ({
             return (
               <div className="flex justify-between">
                 <span className="font-light text-sm text-neutral-cool_gray">{addOnObject.text}</span>
-                <span className="text-primary-marine_blue text-sm desktop:text-base">+${cost}/{period}</span>
+                <span className="text-primary-marine_blue text-sm desktop:text-base">+${cost}/{paymentPeriod}</span>
               </div>
             )
           })}
         </div>
       </div>
-      <div className="flex justify-between p-6">
-        <span className="text-sm desktop:text-base font-light text-neutral-cool_gray">Total (per month)</span>
-        <span className="text-sm desktop:text-2xl text-primary-purplish_blue font-extrabold"> +${totalCost}/month </span>
+      <div className="flex justify-between">
+        <span className="text-sm desktop:text-base font-light text-neutral-cool_gray">Total (per {paymentPeriod})</span>
+        <span className="text-sm desktop:text-xl text-primary-purplish_blue font-extrabold"> ${totalCost}/{paymentPeriod} </span>
       </div>
     </div>
   )
